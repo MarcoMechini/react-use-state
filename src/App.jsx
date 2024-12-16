bdf5c7ac4e781e6ed3eaad340e9e0c78350f6042
Marco Mechini <mechinimarco35@gmail.com>
2024-12-16 22:13:43 +0100

    feat: style complete

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,25 @@ function App() {
 
   return (
     <>
-      {data.map(curPost => {
-        return (
-          <Btn
-            key={curPost.id}
-            post={curPost}
-            // quanto verrà invocata la funzione setTitle come props invocherà la funzione seTitle per aggiornare il valore dello state
-            onClick={() => setCard(curPost)}
-          />)
-      })
-      }
 
-      <div class="counter">{card.title}</div>
-      <div class="counter">{card.description}</div>
+      <h1>Learn Web development</h1>
+      <nav>
+        {data.map(curPost => {
+          return (
+            <Btn
+              key={curPost.id}
+              post={curPost}
+              // quanto verrà invocata la funzione setTitle come props invocherà la funzione seTitle per aggiornare il valore dello state
+              onClick={() => setCard(curPost)}
+            />)
+        })
+        }
+      </nav>
+
+      <div className='content'>
+        <div class="title">{card.title}</div>
+        <div class="desc">{card.description}</div>
+      </div>
     </>
   )
 }
